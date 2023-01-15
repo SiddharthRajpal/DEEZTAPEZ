d3077f0591646567ba8ee3f4f8efc8cc2ac4f70f
dhairyas1 <119152782+dhairyas1@users.noreply.github.com>
2023-01-15 10:47:09 +0530

    Update thisdoesnotwokr.py

--- thisdoesnotwokr.py
+++ thisdoesnotwokr.py
@@ -12,16 +12,34 @@ while True:
     upper = np.array([180, 255, 50])
 
     mask = cv.inRange(img,lower,upper)
-    cv .imshow('wao',mask)
+    
 
 
 
 
     edged= cv.Canny(mask,30,200)
     
-    ret, thresh1 = cv.threshold(img, 50, 200, cv.THRESH_BINARY)
+    ret, thresh1 = cv.threshold(img, 50, 500, cv.THRESH_BINARY)
+    thresh1 = cv.cvtColor(thresh1, cv.COLOR_RGB2GRAY)
     contours, hiearchy = cv.findContours(thresh1,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-    img = cv .drawContours(thresh1, contours, -1, (0,255,255), 3)
+    def get_contour_areas(contours):
+
+        all_areas= []
+
+        for cnt in contours:
+            area= cv.contourArea(cnt)
+            all_areas.append(area)
+
+        return all_areas
+
+
+    sorted_contours= sorted(contours, key=cv.contourArea, reverse= True)
+    cv .imshow('wao',mask)
+
+    largest_item= sorted_contours[0]
+
+    img = cv .drawContours(mask, largest_item, -1, (0,255,255), 3)
+    
     cv.imshow('mask',thresh1)
     cv.imshow('img',img)
     key = cv.waitKey(5)
